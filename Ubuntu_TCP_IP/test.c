dc48d2f2d3e120e2c314597a253d91e5c6a67696
rainymood <tinkle9696@gmail.com>
2020-11-12 18:59:18 -0800

    argv test

---
+++ Ubuntu_TCP_IP/test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[])
+{
+    int temp = 0;
+    int temp2 = 1;
+    printf("argv[0] %s \n", argv[0]);
+
+    printf("argv[1] %s \n", argv[1]);
+    printf("argv[2] %s \n", argv[2]);
+    printf("temp + temp2 = %d \n", temp+temp2);
+
+    return 0;
+}
