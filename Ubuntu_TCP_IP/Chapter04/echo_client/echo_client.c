7fd893b06a575cd54c381f5feedefd72309917ba
rainymood <tinkle9696@gmail.com>
2020-11-13 18:59:18 -0800

    echo client 와 echo_server 예제

--- Ubuntu_TCP_IP/Chapter04/echo_client/echo_client.c
+++ Ubuntu_TCP_IP/Chapter04/echo_client/echo_client.c
@@ -26,9 +26,34 @@ int main(int argc, char* argv[])
     
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr =  inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1) // connect 함수가 호출되고 있다. 앞서 언급했듯이 이 함수호출로 인한 연결요청 정보가
+    //서버의 대기큐에 등록이 되면 connect함수는 정상적으로 호출을 완료한다. 때문에 Connected .................가 호출되고 문자열정보가 출력되더라도 서버에서 accept함수를
+    // 호출하지 않은 상황이라면 실제 서비스가 이뤄지지 않음에 주의해야한다. 
+        error_handling("connect() error");
+    else
+        puts("Connected.............");
     
 
+    while(1)
+    {
+        fputs("Input message(Q to quit): ", stdout);
+        fgets(message, BUF_SIZE, stdin);
 
+        if( !strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+            break;
+        
+        write(sock, message, strlen(message));
+        str_len = read(sock, message, BUF_SIZE -1 );
+        message[str_len] = 0;
+        printf("Message from server: %s", message);
+    }
+
+    close(sock); //이렇게 close함수가 호출되면 상대 소켓으로는 
+    // eof가 전송된다. 즉 eof는 연결의 끝을 의미한다
+    
     return 0;
 }
 
