c64533beb4d4ff8e3dc546e24605769b39f67542
rainymood <tinkle9696@gmail.com>
2020-11-16 06:58:52 -0800

    계산기 테스트중

--- Ubuntu_TCP_IP/Chapter04/operator/op_client/op_client.c
+++ Ubuntu_TCP_IP/Chapter04/operator/op_client/op_client.c
@@ -6,9 +6,84 @@
 #include<sys/socket.h>
 
 #define BUF_SIZE 1024
+
+
+
 void error_handling(char *message);
-int main(int argc, char argv[])
+int main(int argc, char* argv[])
 {
+    int sock;
+    char message[BUF_SIZE];
+    int str_len, recv_len, recv_cnt;
+    struct sockaddr_in serv_adr;
+
+    if(argc != 3)
+    {
+        printf("Usage :%s <IP> <port> \n", argv[0]);
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+        error_handling("connect() error");
+    else
+        puts("Connected.........................");
+
+    
+    puts("Operand count:");
+    int count;
+    scanf("%d", &count);
+
+    // 강제로 1로한다 지금은 테스트라서 
+    count = 1;
+    for(int i = 0; i<count; i++)
+    {
+        printf("Operand %d :", i+1);
+        int number = 11;
+        fputs("숫자 테스트 : \n", stdout);
+        scanf("%d",&number);
+        printf("test %d ", number);
+
+        str_len = write(sock, (char*)&number, sizeof(number));
+
+
+        recv_len = 0;
+        int getServerValue;
+        while(recv_len < sizeof(int))
+        {
+            recv_cnt = read(sock, (char*)(&getServerValue+recv_len), sizeof(int));
+            if(recv_cnt == -1)
+                error_handling("read() error");
+            recv_len += recv_cnt;
+
+        }
+        
+        printf("Message from server: %d ", getServerValue);
+        
+    }
+    close(sock);
+
+/*
+    recv_len = 0;
+        while(recv_len < str_len)
+        {
+            recv_cnt = read(sock, &message[recv_len], BUF_SIZE -1);
+            if(recv_cnt == -1)
+                error_handling("read() error");
+
+            recv_len += recv_cnt;
+        }
+    */
+
+
     return 0;
 }
 
--- Ubuntu_TCP_IP/Chapter04/operator/op_server/op_server.c
+++ Ubuntu_TCP_IP/Chapter04/operator/op_server/op_server.c
@@ -7,8 +7,59 @@
 
 #define BUF_SIZE 1024
 void error_handling(char *message);
-int main(int argc, char argv[])
+int main(int argc, char* argv[])
 {
+    int serv_sock, clnt_sock;
+    char message[BUF_SIZE];
+    int str_len, i;
+
+    struct sockaddr_in serv_adr, clnt_adr;
+    socklen_t clnt_adr_sz;
+
+    if(argc != 2 ){
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1)
+        error_handling("socket() error");
+    
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+        error_handling("bind() error");
+
+    if(listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+
+    clnt_adr_sz = sizeof(clnt_adr);
+
+
+    // 일단 한개 클라만 받아서 인트형 받아서 무조건 1004 반환 해줘보자 
+
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+    if(clnt_sock == -1)
+        error_handling("accept() error");
+    else
+        printf("connected client %d \n", 1);
+    
+    // 클라가준 인트값 먼저 한게 읽자
+    int clientIntValue;  
+    str_len = read(clnt_sock, &clientIntValue, sizeof(int) );
+    printf("str_len %d", str_len);
+
+    printf("clientIntValue %d", clientIntValue);
+    
+    int serverReturnValue = 1004;
+    write(clnt_sock, &serverReturnValue, sizeof(4));
+
+    close(clnt_sock);
+    close(serv_sock);
+
     return 0;
 }
 
